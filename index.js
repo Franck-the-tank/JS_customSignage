69200d44df36c0564bde2ada7daaf5dd06fe13bb
Franck-the-Tank <nussbaumer.franck@gmail.com>
2015-09-11 23:38:01 +0200

    Second commit of this program, to see how to talk with the server

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ var express = require("express"),
 // how you require a node module
 // you can also call in a specific folder ./file.ff
     app = express ()
+    user={}
 
 app.use("/", express.static("./public"))
+
+app.post("/calculate", function(req, res){
+    user=req.body
+    res.send("User name logged")
+});
 
 
 app.listen(3000)
--- public/js/controller.js
+++ public/js/controller.js
@@ -1,8 +1,22 @@
 var app = angular.module('customSignage')
 
 var price = 5,
-    length = word.length(),
+  length = word.length(),
 
-    // but we need to send a post request once the user clicks
-    // in order to put it in a variable here:
-var total;
+  // but we need to send a post request once the user clicks
+  // in order to put it in a variable here:
+  var total;
+
+app.controller('useCtrl', ['$scope', '$http', function($scope, $http) {
+
+      $scope.calculate = function() {
+        $http.post("/calculate", {
+          name: $scope.username
+        }).success(function(data) {
+          alert("got it" + $scope.username)
+        }).console.error(function() {
+          alert("problem")
+        });
+      }
+
+    ]);
